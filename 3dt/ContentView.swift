829bdd005eb5656f9510e6e6119fbbe08249ddc2
qiu-ms <qiumenseng@gmail.com>
2024-01-23 15:29:06 +0100

    update

--- 3dt/ContentView.swift
+++ 3dt/ContentView.swift
@@ -15,28 +15,39 @@ struct ContentView : View {
 }
 
 struct ARViewContainer: UIViewRepresentable {
-    
     func makeUIView(context: Context) -> ARView {
-        
         let arView = ARView(frame: .zero)
 
-        // Create a cube model
-        let mesh = MeshResource.generateBox(size: 0.1, cornerRadius: 0.005)
-        let material = SimpleMaterial(color: .gray, roughness: 0.15, isMetallic: true)
-        let model = ModelEntity(mesh: mesh, materials: [material])
+        let fileURL = FileManager.default.temporaryDirectory.appendingPathComponent(UUID().uuidString)
+                // Download contents of imageURL as Data.  Use a URLSession if you want to do this asynchronously.
+                let data = try! Data(contentsOf: URL(string: "http://192.168.178.92:8080/hello")!)
+                print(data)
+                // Write the image Data to the file URL.
+                try! data.write(to: fileURL)
+        let texture = try? TextureResource.load(contentsOf: fileURL)
 
-        // Create horizontal plane anchor for the content
-        let anchor = AnchorEntity(.plane(.horizontal, classification: .any, minimumBounds: SIMD2<Float>(0.2, 0.2)))
-        anchor.children.append(model)
+                    // Create a material with the texture
+                    var material = SimpleMaterial()
+        material.color.texture = MaterialParameters.Texture(texture!)
 
+                    // Create a plane entity with the material
+                    let plane = ModelEntity(mesh: .generatePlane(width: 0.4, height: 0.4), materials: [material])
+        let sphereAnchor = AnchorEntity(world: SIMD3(x: 0, y: 0, z: 0))
+
+        
+        sphereAnchor.addChild(plane)
+        
         // Add the horizontal plane anchor to the scene
-        arView.scene.anchors.append(anchor)
+        arView.scene.anchors.append(sphereAnchor)
+
 
         return arView
         
     }
     
-    func updateUIView(_ uiView: ARView, context: Context) {}
+    func updateUIView(_ uiView: ARView, context: Context) {
+        
+    }
     
 }
 
